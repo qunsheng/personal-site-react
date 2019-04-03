2516488c4e715421c802022e3c170a5047bfb8a6
Quentin Chen <qunsheng.chen@hotmail.com>
2019-04-03 19:45:02 -0400

    add bubble chart as skill set

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
-import SkillSet from './components/SkillSet'
-import './App.css';
-import WorkExperience from './components/WorkExperience';
+import React, { Component } from "react";
+import SkillSet from "./components/SkillSet";
+import "./App.css";
+import WorkExperience from "./components/WorkExperience";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <header id="showcase" className="grid">
-          <div className="bg-image"></div>
+          <div className="bg-image" />
           <div class="content-wrap">
             <ul>
               <li>
                 <div className="card">
-                  <img width="500" src={require('./images/qunsheng.jpeg')} />
-
+                  <img
+                    width="500"
+                    src={require("./images/qunsheng.jpeg")}
+                    alt=" "
+                  />
                 </div>
-
               </li>
               <li>
                 <h1> Quentin Chen</h1>
-                <p>Over 20 years IT experience. Former heart and soul of NASDAQ biggest trading engine. Original author of many
-                    Wall Street well known applications such as BRASS/BNET. Passionate about JavaScript and web application
-                    development.</p>
+                <p>
+                  Over 20 years IT experience. Former heart and soul of NASDAQ
+                  biggest trading engine. Original author of many Wall Street
+                  well known applications such as BRASS/BNET. Passionate about
+                  JavaScript and web application development.
+                </p>
               </li>
-    
             </ul>
-
-          
-          
           </div>
-          
         </header>
 
         <SkillSet />
         <WorkExperience />
-        
-
-
       </React.Fragment>
     );
   }
