c2e99925112d2523ee54e5688b0b7bc68a1fa4ea
Quentin Chen <qunsheng.chen@hotmail.com>
2019-04-05 20:35:43 -0400

    make landing page cover whole screen

--- src/App.js
+++ src/App.js
@@ -7,29 +7,21 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <header id="showcase" className="grid">
+        <header id="showcase" className="showcase-container">
           <div className="bg-image" />
-          <div class="content-wrap">
-            <ul>
-              <li>
-                <div className="card">
-                  <img
-                    width="500"
-                    src={require("./images/qunsheng.jpeg")}
-                    alt=" "
-                  />
-                </div>
-              </li>
-              <li>
-                <h1> Quentin Chen</h1>
-                <p>
-                  Over 20 years IT experience. Former heart and soul of NASDAQ
-                  biggest trading engine. Original author of many Wall Street
-                  well known applications such as BRASS/BNET. Passionate about
-                  JavaScript and web application development.
-                </p>
-              </li>
-            </ul>
+          <div className="showcase-box" id="bluebox">
+            <div className="showcase-sub-box">
+              <img src={require("./images/qunsheng.jpeg")} alt=" " />
+            </div>
+            <div className="showcase-sub-box">
+              <h1> Quentin Chen</h1>
+              <p>
+                Over 20 years IT experience. Former heart and soul of NASDAQ
+                biggest trading engine. Original author of many Wall Street well
+                known applications such as BRASS/BNET. Passionate about
+                JavaScript and web application development.
+              </p>
+            </div>
           </div>
         </header>
 
